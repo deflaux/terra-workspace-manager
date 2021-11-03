842af145efea16b113b80594d342ab14d73bf392
Yu Hu <yuhuyoyo@google.com>
2021-11-03 10:43:49 -0400

    Add check in the validateBucketName methods following the google cloud guideline https://cloud.google.com/storage/docs/naming-buckets#requirements.

--- service/src/main/java/bio/terra/workspace/service/resource/ValidationUtils.java
+++ service/src/main/java/bio/terra/workspace/service/resource/ValidationUtils.java
@@ -5,14 +5,20 @@ import bio.terra.workspace.generated.model.ApiGcpAiNotebookInstanceCreationParam
 import bio.terra.workspace.generated.model.ApiGcpAiNotebookInstanceVmImage;
 import bio.terra.workspace.service.resource.exception.InvalidNameException;
 import bio.terra.workspace.service.resource.referenced.exception.InvalidReferenceException;
+import com.google.common.collect.ImmutableList;
 import java.util.regex.Pattern;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/** A collection of static validation functions */
+/**
+ * A collection of static validation functions
+ */
 public class ValidationUtils {
+
   private static final Logger logger = LoggerFactory.getLogger(ValidationUtils.class);
+  private static final String GOOG_PREFIX = "goog";
+  private static final ImmutableList<String> GOOGLE_NAMES = ImmutableList.of("google", "g00gle");
 
   /**
    * GCS bucket name validation is somewhat complex due to rules about usage of "." and restricted
@@ -37,7 +43,10 @@ public class ValidationUtils {
   public static final Pattern AI_NOTEBOOK_INSTANCE_NAME_VALIDATION_PATTERN =
       Pattern.compile("(?:[a-z](?:[-a-z0-9]{0,61}[a-z0-9])?)");
 
-  /** Resource names must be 1-1024 characters, using letters, numbers, dashes, and underscores and must not start with a dash or underscore.*/
+  /**
+   * Resource names must be 1-1024 characters, using letters, numbers, dashes, and underscores and
+   * must not start with a dash or underscore.
+   */
   public static final Pattern RESOURCE_NAME_VALIDATION_PATTERN =
       Pattern.compile("^[a-zA-Z0-9][-_a-zA-Z0-9]{0,1023}$");
 
@@ -45,7 +54,32 @@ public class ValidationUtils {
     if (StringUtils.isEmpty(name) || !BUCKET_NAME_VALIDATION_PATTERN.matcher(name).matches()) {
       logger.warn("Invalid bucket name {}", name);
       throw new InvalidReferenceException(
-          "Invalid GCS bucket name specified. Names must be 3-222 lowercase letters, numbers, dashes, and underscores. See Google documentation for the full specification.");
+          "Invalid GCS bucket name specified. Names must be 3-222 lowercase letters, numbers, dashes, and underscores. See Google documentation https://cloud.google.com/storage/docs/naming-buckets#requirements for the full specification.");
+    }
+    String[] substring = name.split("\\.");
+    if (substring.length == 0 && name.length() > 63) {
+      logger.warn("Invalid bucket name {}", name);
+      throw new InvalidReferenceException(
+          "Invalid GCS bucket name specified. Bucket names must contain 3-63 characters. See Google documentation https://cloud.google.com/storage/docs/naming-buckets#requirements for the full specification.");
+    }
+    for (String s : substring) {
+      if (s.length() > 63) {
+        logger.warn("Invalid bucket name {}", name);
+        throw new InvalidReferenceException(
+            "Invalid GCS bucket name specified. Names containing dots can contain up to 222 characters, but each dot-separated component can be no longer than 63 characters. See Google documentation https://cloud.google.com/storage/docs/naming-buckets#requirements for the full specification.");
+      }
+    }
+    if (name.startsWith(GOOG_PREFIX)) {
+      logger.warn("Invalid bucket name {}", name);
+      throw new InvalidReferenceException(
+          "Invalid GCS bucket name specified. Bucket names cannot have prefix goog. See Google documentation https://cloud.google.com/storage/docs/naming-buckets#requirements for the full specification.");
+    }
+    for (String google : GOOGLE_NAMES) {
+      logger.warn("Invalid bucket name {}", name);
+      if (name.contains(google)) {
+        throw new InvalidReferenceException(
+            "Invalid GCS bucket name specified. Bucket names cannot contains google or mis-spelled google. See Google documentation https://cloud.google.com/storage/docs/naming-buckets#requirements for the full specification.");
+      }
     }
   }
 
--- service/src/test/java/bio/terra/workspace/service/resource/referenced/ReferenceValidationUtilsTest.java
+++ service/src/test/java/bio/terra/workspace/service/resource/referenced/ReferenceValidationUtilsTest.java
@@ -9,11 +9,86 @@ import org.junit.jupiter.api.Test;
 
 public class ReferenceValidationUtilsTest extends BaseUnitTest {
 
+  private static final String MAX_VALID_STRING = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+  private static final String INVALID_STRING = MAX_VALID_STRING + "b";
+  private static final String MAX_VALID_STRING_WITH_DOTS =
+      MAX_VALID_STRING + "." + MAX_VALID_STRING + "." + MAX_VALID_STRING + "."
+          + "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+
   @Test
   public void testInvalidCharInBucketName() {
     assertThrows(
         InvalidReferenceException.class,
         () -> ValidationUtils.validateBucketName("INVALIDBUCKETNAME"));
+  }
+
+  @Test
+  public void validateBucketName_nameHas64Character_throwsException() {
+    assertThrows(
+        InvalidReferenceException.class,
+        () -> ValidationUtils.validateBucketName(INVALID_STRING));
+  }
+
+  @Test
+  public void validateBucketName_nameHas63Character_OK() {
+    ValidationUtils.validateBucketName(MAX_VALID_STRING);
+  }
+
+  @Test
+  public void validateBucketName_nameHas2Character_throwsException() {
+    assertThrows(
+        InvalidReferenceException.class,
+        () -> ValidationUtils.validateBucketName("aa"));
+  }
+
+  @Test
+  public void validateBucketName_nameHas3Character_OK() {
+    ValidationUtils.validateBucketName("123");
+  }
+
+  @Test
+  public void validateBucketName_nameHas222CharacterWithDotSeparator_OK() {
+    ValidationUtils.validateBucketName(MAX_VALID_STRING_WITH_DOTS);
+  }
+
+  @Test
+  public void validateBucketName_nameWithDotSeparatorButOneSubstringExceedsLimit_throwsException() {
+    assertThrows(
+        InvalidReferenceException.class,
+        () -> ValidationUtils.validateBucketName(INVALID_STRING + "." + MAX_VALID_STRING));
+  }
+
+  @Test
+  public void validateBucketName_nameStartAndEndWithNumber_OK() {
+    ValidationUtils.validateBucketName("1-bucket-1");
+  }
+
+  @Test
+  public void validateBucketName_nameStartAndEndWithDot_throwsException() {
+    assertThrows(
+        InvalidReferenceException.class,
+        () -> ValidationUtils.validateBucketName(".bucket-name."));
+  }
+
+  @Test
+  public void validateBucketName_nameWithGoogPrefix_throwsException() {
+    assertThrows(
+        InvalidReferenceException.class,
+        () -> ValidationUtils.validateBucketName("goog-bucket-name1"));
+  }
+
+  @Test
+  public void validateBucketName_nameContainsGoogle_throwsException() {
+    assertThrows(
+        InvalidReferenceException.class,
+        () -> ValidationUtils.validateBucketName("bucket-google-name"));
+  }
+
+  @Test
+  public void validateBucketName_nameContainsG00gle_throwsException() {
+    assertThrows(
+        InvalidReferenceException.class,
+        () -> ValidationUtils.validateBucketName("bucket-g00gle-name"));
   }
 
   @Test
